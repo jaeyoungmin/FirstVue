74a2c6af3ffa96958131c66d64fe11c393da8060
jaeyoungmin <jaeyoung8456@naver.com>
2020-10-08 14:17:32 +0900

    fixed file

---
+++ src/components/gallery/ImageCard.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="image-card" @mouseover="mouseover" @mouseleave="mouseleave">
+    <v-fade-transition>
+      <div v-if="isMouseOver" class="cover">
+        <v-row justify="center" align="center" style="height:100%">
+          <div class="content-wrapper">
+            <v-avatar>
+              <v-icon dark large>mdi-link</v-icon>
+            </v-avatar>
+            <h4>{{ text }}</h4>
+          </div>
+        </v-row>
+      </div>
+    </v-fade-transition>
+    <v-img :src="url" alt="3" aspect-ratio="1.7" class="img"></v-img>
+    <!-- <v-hover v-slot:default="{ hover }">
+      <v-img :src="url" alt="3" aspect-ratio="1.7" class="img">
+        <v-fade-transition>
+          <div
+            v-if="hover"
+            class="d-flex flex-column transition-fast-in-fast-out orange darken-2 v-card--reveal"
+          >
+            <v-icon large color="white" class="icon">mdi-link</v-icon>
+            <h3>
+              {{ text }}
+            </h3>
+            <p class="un">Unsplash</p>
+          </div>
+        </v-fade-transition>
+      </v-img>
+    </v-hover> -->
+  </div>
+</template>
+
+<script>
+// import { mapState } from "vuex";
+export default {
+  name: "ImageCard",
+  // computed: { ...mapState(["images"]) }
+  props: ["url", "text"],
+  data() {
+    return {
+      isMouseOver: false
+    };
+  },
+  methods: {
+    mouseover() {
+      this.isMouseOver = true;
+      //console.log("up");
+    },
+    mouseleave() {
+      //console.log("out");
+      this.isMouseOver = false;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+/*.v-card--reveal {
+  height: 100%;
+  transform-origin: center top;
+  align-items: center;
+  justify-content: center;
+  opacity: 0.8;
+  width: 100%;
+}
+
+.un {
+  font-size: 0.8em;
+}*/
+.image-card {
+  position: relative;
+}
+.content-wrapper {
+  text-align: center;
+}
+.cover {
+  background-color: #efa053;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+  opacity: 0.8;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
+.fade-enter,
+.fade-laeve-to {
+  opacity: 0;
+}
+</style>
---
+++ src/views/Menu.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="menu">
+    <v-row justify="center">
+      <v-expansion-panels popout>
+        <v-expansion-panel v-for="(item, i) in items" :key="i">
+          <v-expansion-panel-header>
+            {{ item.title }}
+            <v-spacer></v-spacer>
+            <v-icon>mdi-link</v-icon>
+          </v-expansion-panel-header>
+          <v-expansion-panel-content
+            >{{ item.text }}
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "menu",
+  data() {
+    return {
+      items: [
+        {
+          title: "Solutions Based",
+          text: "ssqweqsds"
+        },
+        {
+          title: "djfhdk",
+          text: "sqwezzxfasdasqwess"
+        },
+        {
+          title: "Solutios.",
+          text: "ssvzxc vzxcvdfsfs"
+        },
+        {
+          title: "Solused.",
+          text: "sfqweq rwqzxcvss"
+        },
+        {
+          title: "Solut",
+          text: "ssssdaasdadasd"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
